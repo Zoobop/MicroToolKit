2aa7a4ed52538b5e378319cdd03ff6f43acdb9dd
Zoobop <86850448+Zoobop@users.noreply.github.com>
2022-05-01 20:54:09 -0500

    Added Parse and TryParse functions to StringUtils.h

--- ModernDT/Examples/main.cpp
+++ ModernDT/Examples/main.cpp
@@ -6,34 +6,7 @@
 
 int main() 
 {
-    auto map = mtk::Map<uint32_t, Player>([](uint32_t id){ return id; });
-    map.Reserve(10);
+    auto result = mtk::Parse<float>("1");
 
-    map.Insert(0, Player { "Bellona", 10, Player::Warrior });
-    map.Insert(1, Player { "Poseidon", 20, Player::Mage });
-    map.Insert(2, Player { "Ymir", 30, Player::Guardian });
-    map.Insert(3, Player { "Hou Yi", 40, Player::Hunter });
-    map.Insert(4, Player { "Tsukuyomi", 50, Player::Assassin });
-
-    /*map.InsertRange({
-        { 0, Player { "Bellona", 10, Player::Warrior }},
-        { 1, Player { "Poseidon", 20, Player::Mage }},
-        { 2, Player { "Ymir", 30, Player::Guardian }},
-        { 3, Player { "Hou Yi", 40, Player::Hunter }},
-        { 4, Player { "Tsukuyomi", 50, Player::Assassin }}
-    });*/
-
-    LOG(map);
-
-    LOG(map.At(0));
-    LOG(map.At(1));
-    LOG(map.At(2));
-    LOG(map.At(3));
-    LOG(map.At(4));
-
-    LOG(map.Find(0));
-    LOG(map.Find(1));
-    LOG(map.Find(2));
-    LOG(map.Find(3));
-    LOG(map.Find(4));
+    LOG(result);
 }
