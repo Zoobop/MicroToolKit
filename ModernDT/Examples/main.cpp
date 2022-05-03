1ad815901fc690d431ed47815a591932ddf293d4
Zoobop <86850448+Zoobop@users.noreply.github.com>
2022-05-03 02:00:36 -0500

    Added the beginning of a string class

--- ModernDT/Examples/main.cpp
+++ ModernDT/Examples/main.cpp
@@ -1,15 +1,16 @@
 #include "mtkpch.h"
 
+#include "Common/String.h"
 #include "Example.h"
 #include "File/FileHandler.h"
 #include "Structures/Map.h"
 
 int main() 
 {
-    auto num = 0;
-    if (TryParse("10", num))
-    {
-        LOG(num);
-    }
-    LOG("Cannot parse!");
+	//mtk::string name;
+	//mtk::string other = "None";
+
+	//name = other;
+
+	//std::cout << "\"" << other << "\"";
 }
