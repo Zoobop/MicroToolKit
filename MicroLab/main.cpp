d486cb0877e590abab1e86fe424449364874c3ae
Zoobop <86850448+Zoobop@users.noreply.github.com>
2022-11-01 15:13:37 -0500

    Added test runner base class and set up solution for testing

---
+++ MicroLab/main.cpp
@@ -0,0 +1,7 @@
+#include "TestRunner.h"
+
+int main(int argc, char* argv[])
+{
+    TestRunner<int> tr;
+    return 0;
+}
---
+++ MicroLab/src/TestRunner.h
@@ -0,0 +1,6 @@
+#pragma once
+
+template<typename _Type>
+class TestRunner
+{
+};
